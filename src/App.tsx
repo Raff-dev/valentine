ccbbdef17dd46715631fe43f41a2a8b6a563a2be
Rafał Łazicki <rafal.lazicki99@gmail.com>
2024-02-14 03:01:53 +0100

    fix statics and add deployment script

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,23 @@ import { useState } from "react";
 import Snowfall from 'react-snowfall';
 import { animated, useSpring } from "react-spring";
 
+import snowflake2Img from "../assets/img/heart.png";
+import snowflake1Img from "../assets/img/horse.png";
+import horsesImg from "../assets/img/horses.webp";
+import picImg from "../assets/img/pic.webp";
+
 const snowflake1 = document.createElement("img");
 const snowflake2 = document.createElement("img");
 
-snowflake1.src = "/public/horse.png";
-snowflake2.src = "/public/heart.png";
+snowflake1.src = snowflake1Img;
+snowflake2.src = snowflake2Img;
 
 const images = [snowflake1, snowflake2];
 
 const App = () => {
   const [showFinalMessage, setShowFinalMessage] = useState(false);
   const [noButtonStyle, setNoButtonStyle] = useState({});
-  const [imagePath, setImagePath] = useState("public/pic.webp");
+  const [imagePath, setImagePath] = useState(picImg);
 
   const yesAnimation = useSpring({
     opacity: showFinalMessage ? 1 : 0,
@@ -43,7 +48,7 @@ const App = () => {
   const handleYesButtonClick = () => {
     console.log("clicked yes 😊");
     setShowFinalMessage(true);
-    setImagePath("public/horses.webp");
+    setImagePath(horsesImg);
   };
 
   return (
@@ -86,21 +91,21 @@ const App = () => {
           </Grid>
         </>
       )}
-      <Snowfall
-        // Applied to the canvas element
-        style={{
-          position: "fixed",
-          width: "100vw",
-          height: "100vh",
-        }}
-        // Controls the number of snowflakes that are created (default 150)
-        snowflakeCount={15}
-        // Pass in the images to be used
-        images={images}
-        radius={[20, 80]}
-      />
       {showFinalMessage && (
         <>
+          <Snowfall
+            // Applied to the canvas element
+            style={{
+              position: "fixed",
+              width: "100vw",
+              height: "100vh",
+            }}
+            // Controls the number of snowflakes that are created (default 150)
+            snowflakeCount={15}
+            // Pass in the images to be used
+            images={images}
+            radius={[20, 80]}
+          />
           <animated.div style={yesAnimation}>
             <Typography variant="h5" gutterBottom>
               Awesome 😊
